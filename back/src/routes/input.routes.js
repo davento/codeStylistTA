1e78446d3a5f9a898f9a105ce5318afef1c66800
davento <daniela.vento@utec.edu.pe>
2024-12-03 18:21:54 -0500

    deleted backend javascript files

--- back/src/routes/input.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { processInput } from "../controllers/input.controller.js";
-
-const router = Router();
-
-// == Function to process the input
-router.get("/input", processInput);
-
-export default router;
