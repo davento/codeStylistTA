52bdc6b1e9dab6f2369aebb5e0ad661d01c5f735
davento <daniela.vento@utec.edu.pe>
2024-12-13 18:34:36 -0500

    added proglangs and courses jsons

--- front/src/app/modules/analyze/components/analyze/analyze.component.ts
+++ front/src/app/modules/analyze/components/analyze/analyze.component.ts
@@ -5,6 +5,12 @@ import { Course } from 'src/app/shared/interfaces/Course';
 import { Feedback } from 'src/app/shared/interfaces/Feedback';
 import { ProgLanguage } from 'src/app/shared/interfaces/ProgLanguage';
 
+// json imports
+import coursesJSON from "src/app/shared/data/courses.json";
+import progLangsJSON from "src/app/shared/data/programming_languages.json";
+import formatsJSON from "src/app/shared/data/formats.json";
+import tonesJSON from "src/app/shared/data/tones.json";
+
 @Component({
   selector: 'app-analyze',
   templateUrl: './analyze.component.html',
@@ -20,85 +26,10 @@ export class AnalyzeComponent {
   success: boolean = false;
   incorrectInputMessage: string = "Incorrect input, please submit code on the appropriate language.";
 
-  // csv/json file + inputService to load them
-  progLangs: ProgLanguage[] = [
-    {
-      name: "Python",
-      extensions: ["py"],
-      reference_material: ["https://peps.python.org/pep-0008/"]
-    },
-    {
-      name: "C++",
-      extensions: ["cpp", "hpp", "h"],
-      reference_material: ["https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines"]
-    },
-    {
-      name: "JavaScript",
-      extensions: ["js"],
-      reference_material: ["https://developer.mozilla.org/en-US/docs/MDN/Writing_guidelines/Writing_style_guide/Code_style_guide/JavaScript"]
-    },
-    {
-      name: "Java",
-      extensions: ["java"],
-      reference_material: ["https://github.com/piranhacloud/piranha/blob/current/CODE_CONVENTIONS.md"]
-    },
-    {
-      name: "C",
-      extensions: ["c", "h"],
-      reference_material: ["https://www.cs.purdue.edu/homes/cs240/code.html"]
-    },
-    {
-      name: "Rust",
-      extensions: ["rs"],
-      reference_material: ["https://doc.rust-lang.org/nightly/style-guide/"]
-    }
-  ];
+  // csv/json file
+  progLangs: ProgLanguage[] = progLangsJSON;
 
-  courses: Course[] = [
-    {
-      code: 15900,
-      name: "C Programming",
-      description: "Fundamental principles, concepts, and methods of programming in C, with emphasis on applications in the physical sciences and engineering. Basic problem solving and programming techniques; fundamental algorithms and data structures; and use of programming logic in solving engineering problems. Students are expected to complete assignments in a collaborative learning environment.",
-      learning_outcomes: [
-        "Understand the fundamental principles, concepts and methods of computer programming.",
-        "Use the C programming language to developing solutions in the domains of physical sciences, mathematics, and engineering.",
-        "Demonstrate the ability to function as part of a technical team to generate a solution to a programming problem.",
-        "Analyze alternative algorithm designs to implement solutions that make efficient use of limited resources of the computer."
-      ],
-    },
-    {
-      code: 17600,
-      name: "Data Engineering In Python",
-      description: "The course introduces students to programming fundamentals in Python, including loops, functions and different data types, and provides an introduction to data engineering including working with common data formats and learning the basics of data wrangling. Students will format, extract, clean, filter, transform, search, combine, summarize, aggregate, and visualize a diverse range of data sets. Python libraries including MatPlotLib and Pandas are used.",
-      learning_outcomes: [
-        "Write Python code using loops, decision statements, and functions.",
-        "Explain how arguments are passed in Python functions and how the scope of variables impacts execution.",
-        "Use the operations on lists, tuples, and dictionaries to perform appropriate data manipulations.",
-        "Using Matplotlib, create informative plots and other data visualizations. Explain the key qualities of good visualizations.",
-        "Creating and manipulating DataFrames using Pandas.",
-        "Create Python code as well as methods in Pandas to select, search, change, and summarize data in tables.",
-        "Explain how to identify and fill in missing values in data." ,
-        "Apply Pandas functions combine and merge datasets, perform a range of data aggregations, groupings and cross tabulations.",
-        "Given multiple data sets, demonstrate how to summarize, transform, combine the data sets, and aggregate and visualize the resulting data set."
-      ],
-    },
-    {
-      code: 18000,
-      name: "Problem Solving And Object-Oriented Programming",
-      description: "Problem solving and algorithms, implementation of algorithms in a high level programming language, conditionals, the iterative approach and debugging, collections of data, searching and sorting, solving problems by decomposition, the object-oriented approach, subclasses of existing classes, handling exceptions that occur when the program is running, graphical user interfaces (GUIs), data stored in files, abstract data types, a glimpse at topics from other CS courses. Intended primarily for students majoring in computer sciences.",
-      learning_outcomes: [
-        "Understand solving problems by analyzing the problem, designing an algorithm, and programming the solution."
-      ]
-    },
-    {
-      code: 25100,
-      name: "Data Structures And Algorithms",
-      description: "Running time analysis of algorithms and their implementations, one-dimensional data structures, trees, heaps, additional sorting algorithms, binary search trees, hash tables, graphs, directed graphs, weighted graph algorithms, additional topics.",
-      learning_outcomes: [
-        "Understand fundamental data structures, fundamental algorithms, and their implementation."
-      ]
-    }
-  ];
+  courses: Course[] = coursesJSON;
 
   tones: string[] = [
     "Formal",
@@ -113,9 +44,11 @@ export class AnalyzeComponent {
   constructor (
     private fb: FormBuilder,
     private inputService: InputService
-    // add configService when its created
   ) {
-
+    console.log(progLangsJSON);
+    console.log(coursesJSON);
+    console.log(formatsJSON);
+    console.log(tonesJSON);
   }
 
   codeForm: FormGroup = this.fb.group({
